ec3d65beb4665ee304a0e266b1765c0840abf728
Vinicius Scampini <viniciuscscampini@gmail.com>
2021-07-28 10:10:54 -0300

    Modificado o extrato e adicionado css a passina.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,8 @@ export class AppComponent {
 
   transferir($event: any) {
     console.log($event)
-    this.transferencias.push($event); 
+    const transferencia = {...$event, data: new Date()};
+    this.transferencias.push(transferencia); 
     
   }
 
